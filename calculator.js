6ef4d2cdffb16dc8b3681749b782ecca03294eb6
vedantkhursange <140951971+vedantkhursange@users.noreply.github.com>
2023-09-23 20:46:53 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+let input = '';
+
+function appendToDisplay(value) {
+    input += value;
+    display.value = input;
+}
+
+function clearDisplay() {
+    input = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        input = eval(input);
+        display.value = input;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
